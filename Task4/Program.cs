f886a4d813d18da19fa7f3331cd4519ce7a9d728
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-01 15:03:15 +0300

    modified:   Task4/Program.cs

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,5 +1,9 @@
 ﻿Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine());
-int remain = num % 2;
-if (remain == 0) Console.WriteLine("Число " + num + " четное!");
-else Console.WriteLine("Число " + num + " нечетное!");
+int remain = 2;
+
+while (num >= remain)
+{
+    Console.Write(remain + " ");
+    remain = remain + 2;
+}
