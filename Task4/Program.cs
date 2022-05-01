43b0807d6aa5875f0eb0572153e2127f8b6a6216
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-01 21:45:02 +0300

    modified:   Task4/Program.cs

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Write("Введите число: ");
 int num = int.Parse(Console.ReadLine());
-int remain = 2;
+int even = 2;
 
-while (num >= remain)
+while (num >= even)
 {
-    Console.Write(remain + " ");
-    remain = remain + 2;
+    Console.Write(even + " ");
+    even = even + 2;
 }
