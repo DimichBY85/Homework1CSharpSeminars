35eea8b34e0ce063f5063c918e1c17344c1a227d
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-01 09:16:28 +0300

    new file: Task4/Program.cs

---
+++ Task4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int remain = num % 2;
+if (remain == 0) Console.WriteLine("Число " + num + " четное!");
+else Console.WriteLine("Число " + num + " нечетное!");
