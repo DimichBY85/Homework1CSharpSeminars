37207eef7517893670fb4cf96fdd4f14830a7917
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-01 08:09:11 +0300

    new file:   Task2/Program.cs new file:   Task2AlternativeSolution/Program.cs

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите три числа, и узнайте, ");
+Console.WriteLine("какое из них имеет максимальное значение.");
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+if ()
+
+
