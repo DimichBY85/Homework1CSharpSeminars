5d4fb398272ead3225379e8c4f4df907a33294c0
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-01 09:04:02 +0300

    new file: Task3/Program.cs

---
+++ Task3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число, чтобы узнать четное оно или нет: ");
+int num = int.Parse(Console.ReadLine());
+int remain = num % 2;
+if (remain == 0) Console.WriteLine("Число " + num + " четное!");
+else Console.WriteLine("Число " + num + " нечетное!");
